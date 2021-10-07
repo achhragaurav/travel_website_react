d650d3a48d4bc588866f105da1870a46b1469793
Gaurav Achhra <achhragaurav@gmail.com>
2021-10-07 19:28:50 +0530

    bugs

--- src/components/MainPageComp/homedata.js
+++ src/components/MainPageComp/homedata.js
@@ -4,7 +4,7 @@ export const homeData = [
     description: `This is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasiThis is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasi`,
-    image: `https://lh3.googleusercontent.com/RFrz0UPVPhDBwJvrnpxOG8It_L362jkhheViMZcW2spYpci8VQiV7B1VuBR6dGzGPaqyaIlg_C-d60a1R_Vv9PvZGNvMJ02b0oS1NXaXzXIGuIpGXT_V7ewbVi2EWZpZTPW7sliQ4acKThfs_jOPpJtHlpvK6_HC2ho7P9TuR2V0qxOFbZNCXRdBhVUoEfCYtoInOFumLTKhHz9Ss6Aqx9fvGOXy5_hmHe2Bc6UGXje-LhnlHsS33R0NXbQcQhxVWE11O1um8O8nWZujuOcUXIp4g6zI4FeUsafyiFNWEpblR9zPSzoQIn9VOnQTjEUGOLzJoHAo7QoT9HEFtyoBzJaS8CndTNEBO5UvgqwfBUfFMGIhyV9XTHW9EZ0FnwD_bhRu_DCmyRyeStdPcVYiJCsPdFj2rFhh-C3M2uR2c15TzUwo7BOfjcOkAEzPV_12lj3Z8MIXZGUrfLCOgna-cDgf1EBn3RZdgl9jGkdYEd8Di1vdd96D47Xi6VBmXZot27S8NPbpbWWKaQSSQWetRtGWLPJFiBkhNloDcsDv-giJ9XaobnXX5oRNnQ8FBbMEIQBWRqcyfJcZslZPoOSTX-7UqC-IXjP8hn5bZuFCko7F5N93BY1bZ9wJSjmYgGnWYkI5l9Pf1947j-gpxUcFZT0UaLgZ2ZDFDuu1O1tcAboK__AL1yfZV6OQsON599YwX36vgWziLD475AdS_S-bZEsm=w800-h450-no?authuser=1`,
+    image: `https://images.pexels.com/photos/1660995/pexels-photo-1660995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
     moreImages: [
       "https://images.pexels.com/photos/2635616/pexels-photo-2635616.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       "https://images.pexels.com/photos/4386973/pexels-photo-4386973.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -15,7 +15,7 @@ export const homeData = [
     description: `This is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasiThis is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasi`,
-    image: `https://i.ibb.co/Gc3m8Kd/ezgif-com-gif-maker-3.gif`,
+    image: `https://images.pexels.com/photos/415611/pexels-photo-415611.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
     moreImages: [
       "https://images.pexels.com/photos/1618423/pexels-photo-1618423.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       "https://images.pexels.com/photos/3591074/pexels-photo-3591074.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -26,7 +26,7 @@ export const homeData = [
     description: `This is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasiThis is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasi`,
-    image: `https://lh3.googleusercontent.com/xkII2sk7da7lzQWr5SnTcbNWiYbI4kWuhdm8utk6Sb1uTfITH0yaQoanb5ubdrBxx3OsWXhAlMXw_Z1PMMODt8IsPoPafAbXAgOXM5ZkGWmfdjvuDpIDZDs0UZFIajnzNJ5I3GipRaAN1_mmMx2p5p2XYN7SJFFfIlSGLwjWgVlqsyg7-Eo8k2Mhkd0uqPJJi93dLXb8SvbRUKRdhw7mOg5Z1aMgnxj5b5fuCdO3b0gt5yngvS5LxMYZfb4UKeLUPa4Ho0XWJhaGR7WwRcMwbeTy-j8nQaezuXDGRQ27xiowB47tFCCTToajp3x_B24FTxqINg5eL0wc08bwQHTKAfl3rNY3CxKmdblxPrc24YftGjZ18IyY38F84W2SRnvmiqXPrgXAPAyH77Ht0OpwLm8NfYJf7-z0Y_fJAQ7DjZJLfKZ_sWB_XpXcQy_1THCLKtJD8QMA3fEac6TQiKbXN2Kq0H0xPMFZ-YItosOJuqA9JZW5jlG2To_VvKBieF8Sd9kCRpStx5k9zOMjtu_ptLmMjrLj_RBDC2Xc3lCE-Z-0dzu_vuM5DSUyeeqKjzB8FkYGx9VxCxkr3CguQyK2fm_Lwm4gApILc1qyWr6JcyTjgIhnHSnSreI-4t5kpVbU6emgyl-MKLYfT1wGFX6b6K2X-u5RV5kwVx_NjNtGwMecF80vHBhV2U1YzfyI8fHC6IQLPeCOhhJz9Chfz3KYgb8u=w399-h224-no?authuser=1`,
+    image: `https://images.pexels.com/photos/4032062/pexels-photo-4032062.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
     moreImages: [
       "https://images.pexels.com/photos/2860061/pexels-photo-2860061.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       "https://images.pexels.com/photos/4032062/pexels-photo-4032062.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -37,7 +37,7 @@ export const homeData = [
     description: `This is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasiThis is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasi`,
-    image: `https://lh3.googleusercontent.com/1OxEL40fcwK4heLJiEC1pM5dA8YAc9Jw4p7oMie0IAnw7ftWMwmSPjXrKM-HIT2_0rFXHOjDp0-gCn737W4DJeAJd89-9KQ0uK0ekDy3ZjZ-BEeJSHoTP2Uss1tRPLsodkWv4nZ4cBcHP8yfTOHjQC1ZxLtLIZ1xK9yTx5sokt1jPR2wN4AAE1xs5tkkw1XnsR9a6CttfWwYy_vRaZZ8-zWp48dZ_-cjYyNYU-QzC3oCEiH6sFgFtnk0_0WpGltS5cM2H5ultkhVBf57zO7w80aqtmLLeZGePFMSSwqUfzWlzQUwZ4m9Qgx2eIVeAVpDKAfUwWdHVVr0r_EhNp9LG-cFykUZIik9J8IszR5ratyVBuILuo5aO_JsG_Rk083OQZM79H8P6pKls2OQUgJvh0naliSdn78zTlDkuLObuXXP2RfAriJm_lCsUbe9nvVDT_mwq8LL3cPFgJjdK5pwkBgTkVdOiCU7ful4uKfTndyS0cA7mmU4JvbXJvXi9OKoCRM3zrs27UP5Xnd4tr3TgEe14yPmlmjUN1aI-tHzwtSttzLrojGdDloDIMuYqXdu9XcGy5EQZog0p40aZMRqupaIeIv1i605fM6kIwpeDS3L4J3WbL4ngNBlvIwo0QbkzocoygEuMtAkACtrFJod2lHbgfyx_J4FhIf-0rxSLHbglGCQHJs_wYkxxEOV4Then01DvA3fSnUwCPMTeESj3YgL=w398-h224-no?authuser=1`,
+    image: `https://images.pexels.com/photos/1963082/pexels-photo-1963082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
     moreImages: [
       "https://images.pexels.com/photos/460659/pexels-photo-460659.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       "https://images.pexels.com/photos/1963082/pexels-photo-1963082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -48,7 +48,7 @@ export const homeData = [
     description: `This is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasiThis is a beautiful location according to the people Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit suscipit
     autem, voluptates laudantium dolorem beatae rem eos libero explicabo quasi`,
-    image: `https://i.ibb.co/T0nFY6J/ezgif-com-gif-maker.gif`,
+    image: `https://images.pexels.com/photos/574313/pexels-photo-574313.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
     moreImages: [
       "https://images.pexels.com/photos/2382783/pexels-photo-2382783.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       "https://images.pexels.com/photos/2635390/pexels-photo-2635390.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
